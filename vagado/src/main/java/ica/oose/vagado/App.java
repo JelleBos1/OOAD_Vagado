77083caf88e8912c38ff0aaa8711125493d27b26
Alex Post <alexpost1995@gmail.com>
2020-03-29 12:59:33 +0200

    Initial project

---
+++ vagado/src/main/java/ica/oose/vagado/App.java
@@ -0,0 +1,13 @@
+package ica.oose.vagado;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
